02d916614d1f768a8242e0c9a6b3301abd5a06f2
H6NG <8.h.a.n.g.l.i.u.8@gmail.com>
2025-11-07 01:43:38 -0500

    all testcase passed

--- src/main/java/cpen221practice/Resistor.java
+++ src/main/java/cpen221practice/Resistor.java
@@ -2,10 +2,33 @@ package cpen221practice;
 
 class Resistor {
     int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String[] list = {"black",
+                "brown",
+                "red",
+                "orange" ,
+                "yellow" ,
+                "green" ,
+                "blue" ,
+                "violet" ,
+                "grey" ,
+                "white"
+        };
+
+        int index =0;
+
+        for(int i=0; i < list.length; i++) {
+
+            if(list[i].equals(color)){
+                return i;
+            }
+        }
+
+        return index;
     }
 
     String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String[] list = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+
+        return list;
     }
 }
